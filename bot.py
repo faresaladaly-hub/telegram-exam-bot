3539d641216179217e22b7176bddf68b3ac3ec1b
faresaladaly-hub <faresaladaly@gmail.com>
2026-03-14 21:43:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
+import os
 import telebot
 from openai import OpenAI
 
-TOKEN = "PUT_TELEGRAM_TOKEN"
-OPENAI_KEY = "PUT_OPENAI_KEY"
+TOKEN = os.getenv("BOT_TOKEN")
+OPENAI_KEY = os.getenv("OPENAI_KEY")
 
 bot = telebot.TeleBot(TOKEN)
 client = OpenAI(api_key=OPENAI_KEY)
 
 @bot.message_handler(commands=['start'])
 def start(msg):
-    bot.reply_to(msg,"أرسل كتاب أو سلايدات واسألني منها")
+    bot.reply_to(msg,"اهلا 👋 اكتب سؤالك")
 
 @bot.message_handler(func=lambda m: True)
 def chat(msg):
@@ -17,6 +18,7 @@ def chat(msg):
         model="gpt-4o-mini",
         messages=[{"role":"user","content":msg.text}]
     )
+
     bot.reply_to(msg,response.choices[0].message.content)
 
 bot.infinity_polling()
